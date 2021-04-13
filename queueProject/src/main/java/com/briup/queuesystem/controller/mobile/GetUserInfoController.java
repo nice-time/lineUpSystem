cc945d03c2a613daaeb330c6fca043f4e341d556
liuxy <liuxy057@163.com>
2021-04-13 14:02:12 +0800

    test

--- queueProject/src/main/java/com/briup/queuesystem/controller/mobile/GetUserInfoController.java
+++ queueProject/src/main/java/com/briup/queuesystem/controller/mobile/GetUserInfoController.java
@@ -1,8 +1,11 @@
 package com.briup.queuesystem.controller.mobile;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author lijx
  */
+@RequestMapping("")
 public class GetUserInfoController {
 
 }
