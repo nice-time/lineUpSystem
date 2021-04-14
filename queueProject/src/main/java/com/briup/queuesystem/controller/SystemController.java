1a2767f8fd21fba1db9321c5ddc7b1af4962b590
liushaoxiong <jjy940618>
2021-04-14 21:07:14 +0800

    init   202104142107

---
+++ queueProject/src/main/java/com/briup/queuesystem/controller/SystemController.java
@@ -0,0 +1,14 @@
+package com.briup.queuesystem.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SystemController {
+
+
+    public JSONObject get(){
+
+        return null;
+    }
+}
