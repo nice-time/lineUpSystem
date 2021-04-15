d6f48497ffb85032b1b7446df6edcf75724c2fcb
liuxy <liuxy057@163.com>
2021-04-15 17:36:52 +0800

    2021年4月15日17:36:15
                      ,;,,;
                     ,;;'(    马
           __      ,;;' ' \   ┇
        /'  '\'~~'~' \ /'\.)  到
     ,;(      )    /  |.      ┇
    ,;' \    /-.,,(   ) \     成
         ) /       ) / )|     ┇
         ||        ||  \)     功
         (_\       (_\

--- queueProject/src/main/java/com/briup/queuesystem/controller/mobile/GetUserInfoController.java
+++ queueProject/src/main/java/com/briup/queuesystem/controller/mobile/GetUserInfoController.java
@@ -27,33 +27,51 @@ public class GetUserInfoController {
   @Resource
   private UserInfoService userInfoService;
 
+  /**
+   * 用户取号接口
+   * @param jsonObject  入参：phoneNum 手机号码，peopleNum 就餐人数
+   * @return  成功取号 或 异常失败 信息
+   */
   @PostMapping("getUserInfo")
   public Message getUserInfo(@RequestBody JSONObject jsonObject) {
+    //  新建一个List为了返回值的格式
     List<ReslineInfo> reslineInfoList = new ArrayList<>();
     try {
+      //  如果jsonObject的大小为0，说明入参异常，直接返回
       if (jsonObject.size() == 0) {
         return MessageUtil.error("非法入参，请联系管理员");
       }
+      //  从JSONObject入参中，取出电话号码 phoneNum
       String phoneNum = jsonObject.getString("phoneNum");
+      //  对电话号码进行正则校验，判断是否符合规则。符合：下一步 不符合：返回异常信息
       if (!legalMatch.isPhoneLegal(phoneNum)) {
         return MessageUtil.error("请输入正确的手机号码");
       }
+      //  从JSONObject入参中，取出就餐人数 peopleNum
       String peopleNum = jsonObject.getString("peopleNum");
+      //  正则判断 入参peopleNum字符串内容是否 是数字。是：继续 不是：返回异常信息
       if (!legalMatch.isNumber(peopleNum)) {
         return MessageUtil.error("请输入正确的人数");
       }
-      //  应该先搜索改手机号下的 就餐信息是否 有状态为 0 的待就餐信息，有：不能取号，没有：可以取号
+      //  调用方法查询 入参的手机号码是否已经取过号，若取过且状态为待就餐（排队中）。则不插入新的取号信息，返回旧的取号信息，以供展示
       ReslineInfo isWaituser = userInfoService.searchWaitUserInfoByPhoneNumber(phoneNum);
       reslineInfoList.add(isWaituser);
+      //  ReslineInfo 不为空说明 该手机号已经取过号并且没有就餐，返回信息。
       if (isWaituser != null) {
         return MessageUtil.success("您已取号，正在排队中", reslineInfoList);
       }
+      //  将人数转为int类型
       int peopleNumInt = Integer.parseInt(peopleNum);
+      //  新建一个 reslineinfo 对象
       ReslineInfo reslineInfo = new ReslineInfo();
+      //  设置电话号码
       reslineInfo.setPhone(phoneNum);
+      //  设置就餐人数
       reslineInfo.setPeople(String.valueOf(peopleNumInt));
+      //  插入信息
       return userInfoService.InsertUserInfo(reslineInfo);
     } catch (Exception e) {
+      //  捕获异常，打印并曝出异常
       e.printStackTrace();
       return MessageUtil.error("出现异常:" + e.toString());
     }
