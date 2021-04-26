e431beb715fb62c4fb3245afdb55b6a0ff258b4a
liuxy <liuxy057@163.com>
2021-04-26 15:53:40 +0800

    新增MQ叫号接口

--- queueProject/src/main/java/com/briup/queuesystem/rabbitMQ/provider/SendMessageControllerTest.java
+++ queueProject/src/main/java/com/briup/queuesystem/rabbitMQ/provider/SendMessageControllerTest.java
@@ -1,8 +1,12 @@
 package com.briup.queuesystem.rabbitMQ.provider;
 
+import com.alibaba.fastjson.JSONObject;
+import com.briup.queuesystem.utils.Message;
+import com.briup.queuesystem.utils.MessageUtil;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
@@ -13,27 +17,35 @@ import java.util.UUID;
 import org.thymeleaf.util.StringUtils;
 
 @RestController
-@RequestMapping("/RabbitMQTest")
+@RequestMapping("/RabbitMQ")
 public class SendMessageControllerTest {
 
   // 使用RabbitTemplate,这提供了接收/发送等等方法
   @Autowired
   RabbitTemplate rabbitTemplate;
 
-  @GetMapping("/sendDirectMessage")
-  public String sendDirectMessage(String message) {
-    String messageId = String.valueOf(UUID.randomUUID());
-    if (StringUtils.isEmpty(message)) {
-      return "发送消息不能为空！";
+  @RequestMapping("/sendDirectMessage")
+  public Message sendDirectMessage(@RequestBody JSONObject message) {
+    try{
+      String messageId = String.valueOf(UUID.randomUUID());
+      String number = message.getString("number");
+      if (StringUtils.isEmpty(number)) {
+        return MessageUtil.error("叫号失败！号码不能为空。NUMBER="+number);
+      }
+      String createTime = LocalDateTime.now()
+          .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+      Map<String, Object> map = new HashMap<>();
+      map.put("messageId", messageId);
+      map.put("messageData", number);
+      map.put("createTime", createTime);
+      //  应该将叫号信息插入一张表
+      // 将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
+      rabbitTemplate.convertAndSend("TestDirectExchange", "TestDirectRouting", map);
+      return MessageUtil.error("叫号成功");
+    }catch (Exception e){
+      //  捕获异常，打印并曝出异常
+      e.printStackTrace();
+      return MessageUtil.error("出现异常:" + e.toString());
     }
-    String createTime = LocalDateTime.now()
-        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-    Map<String, Object> map = new HashMap<>();
-    map.put("messageId", messageId);
-    map.put("messageData", message);
-    map.put("createTime", createTime);
-    // 将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
-    rabbitTemplate.convertAndSend("TestDirectExchange", "TestDirectRouting", map);
-    return "ok";
   }
 }
